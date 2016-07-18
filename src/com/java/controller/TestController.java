05f572686bbc319269a7c65e255dd7d716b14868
pengyu <1324120475@qq.com>
2016-07-18 13:29:30 +0800

    第一次修改

--- src/com/java/controller/TestController.java
+++ src/com/java/controller/TestController.java
@@ -3,5 +3,7 @@ package com.java.controller;
 public class TestController {
 	public static void main(String[] args) {
 		System.out.println("TestController¿‡...............");
+		System.out.println("123456");
+		System.out.println("654321");
 	}
 }
